ee2169f017d8643c2012ad9e66cbca5d43a1a3c8
Nicola Mometto <brobronsa@gmail.com>
2014-05-21 21:39:59 +0200

    start tools.analyzer.js impl

--- src/main/clojure/clojure/tools/analyzer/js.clj
+++ src/main/clojure/clojure/tools/analyzer/js.clj
@@ -8,8 +8,98 @@
 
 (ns clojure.tools.analyzer.js
   "Analyzer for clojure code, extends tools.analyzer with JS specific passes/forms"
-  (:refer-clojure :exclude [macroexpand-1 macroexpand])
+  (:refer-clojure :exclude [macroexpand-1 var?])
   (:require [clojure.tools.analyzer
              :as ana
-             :refer [analyze analyze-in-env wrapping-meta analyze-fn-method]
-             :rename {analyze -analyze}]))
+             :refer [analyze]
+             :rename {analyze -analyze}]
+            [clojure.tools.analyzer.utils :refer [resolve-var]]
+            cljs.tagged-literals)
+  (:import cljs.tagged_literals.JSValue))
+
+(def specials
+  "Set of the special forms for clojurescript"
+  (into ana/specials '#{ns deftype* defrecord* js*}))
+
+(defmulti parse
+  "Extension to tools.analyzer/-parse for CLJS special forms"
+  (fn [[op & rest] env] op))
+
+(defmethod parse :default
+  [form env]
+  (ana/-parse form env))
+
+(defn empty-env
+  "Returns an empty env map"
+  []
+  {:context    :expr
+   :locals     {}
+   :ns         'cljs.user
+   :namespaces (atom '{cljs.user {:mappings {}
+                                  :aliases  {}
+                                  :ns       'cljs.user}
+                       cljs.core {:mappings {}
+                                  :aliases  {}
+                                  :ns       'cljs.core}})})
+
+(defn desugar-host-expr [[op & expr :as form]]
+  (if (symbol? op)
+    (let [opname (name op)]
+      (cond
+
+       (= (first opname) \.)
+       (let [[target & args] expr
+             args (list* (symbol (subs opname 1)) args)]
+         (with-meta (list '. target (if (= 1 (count args))
+                                      (first args) args))
+           (meta form)))
+
+       (= (last opname) \.)
+       (with-meta (list* 'new (symbol (subs opname 0 (dec (count opname)))) expr)
+         (meta form))
+
+       :else form))
+    form))
+
+(defn macroexpand-1 [form env]
+  (if (seq? form)
+    (let [op (first form)]
+      (if (specials op)
+        form
+        (let [v (resolve-var op env)
+              clj-var (clojure.core/resolve op)]
+          (if (and clj-var
+                   (not (-> env :locals (get op)))
+                   (not v)
+                   (:macro (meta clj-var)))
+            (apply clj-var form env (rest form)) ; (m &form &env & args)
+            (desugar-host-expr form)))))
+        form))
+
+(defrecord Var [name namespace])
+
+(defn create-var
+  [sym {:keys [ns]}]
+  (->Var sym ns))
+
+(defn var? [x]
+  (instance? Var x))
+
+(declare analyze-js-value)
+(defn analyze-form
+  [form env]
+  (if (instance? JSValue form)
+    (analyze-js-value form env)
+    (ana/-analyze-form form env)))
+
+(defn analyze
+  ([form] (analyze form (empty-env) {}))
+  ([form env] (analyze form env {}))
+  ([form env opts]
+     (with-bindings (merge {#'ana/macroexpand-1 macroexpand-1
+                            #'ana/create-var    create-var
+                            #'ana/parse         parse
+                            #'ana/var?          var?
+                            #'ana/analyze-form  analyze-form}
+                           (:bindings opts))
+                    (-analyze form env))))
