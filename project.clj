4b77f7ed5c4b27b403bca3a5655961c8e7e3cc36
Nicola Mometto <brobronsa@gmail.com>
2014-07-21 07:15:40 +0200

    fix resource path

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
-  :resource-paths ["src/cljs"]
+  :resource-paths ["src/main/cljs"]
   :test-paths ["src/test/clojure"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.analyzer "0.3.1-SNAPSHOT"]
