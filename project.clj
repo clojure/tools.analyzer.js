b0a27f819c739fae9ec6ed02046f0485aea4dc94
Nicola Mometto <brobronsa@gmail.com>
2014-08-07 23:03:19 +0200

    update to last cljs

--- project.clj
+++ project.clj
@@ -9,4 +9,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.analyzer "0.5.1-SNAPSHOT"]
                  [org.clojure/tools.reader "0.8.5"]
-                 [org.clojure/clojurescript "0.0-2277"]])
+                 [org.clojure/clojurescript "0.0-2301"]])
