48e69a2e65ee54efa788eaa651fd089b1feb22f8
Nicola Mometto <brobronsa@gmail.com>
2014-08-08 15:39:38 +0200

    bump cljs dep

--- project.clj
+++ project.clj
@@ -8,4 +8,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.analyzer "0.5.1-SNAPSHOT"]
                  [org.clojure/tools.reader "0.8.5"]
-                 [org.clojure/clojurescript "0.0-2301"]])
+                 [org.clojure/clojurescript "0.0-2307"]])
