f2fe64b3f884081eecdf8f8927ef6622bbeaff74
Nicola Mometto <brobronsa@gmail.com>
2014-08-10 00:37:17 +0200

    update quickref

--- src/test/clojure/clojure/tools/analyzer/js/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns clojure.tools.analyzer.js.core-test
-  (:require [clojure.tools.analyzer.js :as js]
-            [clojure.tools.analyzer.env :as env]
-            [clojure.test :refer :all]))
-
-(def env (js/global-env))
-
-(deftest analyzer-test
-  (is (env/with-env env (js/analyze-ns 'cljs.core))))
